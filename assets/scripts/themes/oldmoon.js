34f41136671b69cf29b6b561331cf12134f47f1a
dingdangdog <dingdangdogx@outlook.com>
2025-02-02 14:49:23 +0000

    Deploying to gh-pages from @ dingdangdog/wechat-mdeditor@98c56ce574209c13aa5de485d3b232e23f9cf8e0 🚀

---
+++ assets/scripts/themes/oldmoon.js
@@ -0,0 +1,149 @@
+let oldmoonTheme = {
+  BASE: {
+    "text-align": "left",
+    color: "#595959",
+    "line-height": "1.75",
+  },
+  BASE_BLOCK: {
+    margin: "0.5em 4px",
+  },
+  // block element
+  block: {
+    h1: {
+      "font-size": "1.5em",
+      "text-align": "center",
+      "font-weight": "bold",
+      display: "table",
+      margin: "2em auto 1em auto",
+      padding: "0 1em",
+      "border-bottom": "3px solid #0056b3",
+    },
+    h2: {
+      "font-size": "1.3em",
+      "font-weight": "bold",
+      display: "table",
+      margin: "2em auto 1em auto",
+      padding: "0 1em",
+      "border-bottom": "2px solid #0056b3",
+    },
+    h3: {
+      "font-weight": "bold",
+      "font-size": "1.1em",
+      margin: "2em 8px 0.75em 0",
+      "padding-bottom": ".1em",
+      "padding-left": "8px",
+      "border-bottom": "1px solid #0056b3",
+    },
+    h4: {
+      "font-weight": "bold",
+      "font-size": "1em",
+      margin: "2em 8px 0.5em 8px",
+    },
+    p: {
+      margin: "1.5em 8px",
+      "letter-spacing": "0.1em",
+    },
+    blockquote: {
+      "font-style": "normal",
+      "border-left": "none",
+      padding: "1em",
+      "border-radius": "4px",
+      color: "#FEEEED",
+      background: "rgba(27,31,35,.05)",
+      margin: "2em 8px",
+    },
+    blockquote_p: {
+      "letter-spacing": "0.1em",
+      color: "#555a6b", // 深蓝灰色的衍生色
+      "font-family":
+        "PingFangSC-light, PingFangTC-light, Open Sans, Helvetica Neue, sans-serif",
+      "font-size": "1em",
+      display: "inline",
+    },
+    code: {
+      "font-size": "80%",
+      overflow: "auto",
+      color: "#3a3a55", // 深蓝灰色
+      background: "rgb(247, 247, 247)",
+      "border-radius": "2px",
+      padding: "10px",
+      "line-height": "1.5",
+      border: "1px solid rgb(236,236,236)",
+      margin: "20px 0",
+    },
+    image: {
+      "border-radius": "4px",
+      display: "block",
+      margin: "0.5em auto",
+      width: "100%",
+    },
+    image_org: {
+      "border-radius": "4px",
+      display: "block",
+    },
+    ol: {
+      "margin-left": "0",
+      "padding-left": "1em",
+    },
+    ul: {
+      "margin-left": "0",
+      "padding-left": "1em",
+      "list-style": "circle",
+    },
+    footnotes: {
+      margin: "0.5em 8px",
+      "font-size": "80%",
+    },
+    figure: {
+      margin: "1.5em 8px",
+    },
+  },
+  inline: {
+    // inline element
+    listitem: {
+      "text-indent": "-1em",
+      display: "block",
+      margin: "0.5em 8px",
+    },
+    codespan: {
+      "font-size": "90%",
+      color: "#d14",
+      background: "rgba(27,31,35,.05)",
+      padding: "3px 5px",
+      "border-radius": "4px",
+    },
+    link: {
+      color: "#006d33", // 深蓝绿色
+    },
+    wx_link: {
+      color: "#0056b3", // 深蓝色
+      "text-decoration": "none",
+      "border-bottom": "1px solid #d1e9ff",
+    },
+    strong: {
+      color: "#0056b3",
+      "font-weight": "bold",
+    },
+    table: {
+      "border-collapse": "collapse",
+      "text-align": "center",
+      margin: "1em 8px",
+    },
+    thead: {
+      background: "rgba(0, 0, 0, 0.05)",
+    },
+    td: {
+      "font-size": "80%",
+      border: "1px solid #dfdfdf",
+      padding: "0.25em 0.5em",
+    },
+    footnote: {
+      "font-size": "12px",
+    },
+    figcaption: {
+      "text-align": "center",
+      color: "#888",
+      "font-size": "0.8em",
+    },
+  },
+};
